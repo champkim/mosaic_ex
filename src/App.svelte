1a6f0f022c8be5da97a9315b13324d5f64a8ed4f
hsong <hsong@ontune.co.kr>
2022-09-16 10:39:16 +0900

    button ui 생성

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
+  import Footer from "./Footer.svelte";
   import Mosaic from "./lib/Mosaic.svelte";
   import TopBar from "./lib/TopBar.svelte";
-  
 </script>
 
 <div class="mosaic-ex">
   <TopBar />
   <Mosaic />
+  <Footer />
 </div>
 
 <style>
---
+++ src/Footer.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  let array: number[] = [0, 1, 2, 3, 4, 5];
+  let active = 0;
+
+  const onClickButton = (number: number) => {
+    active = number;
+  };
+</script>
+
+<div class="profile-button">
+  {#each array as name}
+    <button
+      on:click={() => onClickButton(name)}
+      class={`button-num ${active === name ? "active" : ""}`}
+      >{name === 0 ? "Default" : name}</button
+    >
+  {/each}
+</div>
+
+<style>
+  .profile-button {
+    height: 30px;
+    position: absolute;
+    right: 10px;
+    padding-top: 8px;
+  }
+  .button-num {
+    font-size: 16px;
+    background-color: #fff;
+    border: 1px solid rgb(100, 127, 185);
+    border-left: 0;
+    border-radius: 0;
+    widows: 10px;
+    color: black;
+    min-width: 40px;
+  }
+  .button-num.active {
+    background: rgb(100, 127, 185);
+    color: #fff;
+  }
+  .button-num:first-child {
+    border-left: 1px solid rgb(100, 127, 185);
+  }
+</style>
